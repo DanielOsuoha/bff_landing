c25349daae401b3d578ef5445c612aaa857abe58
Daniel Osuoha <dan646655@gmail.com>
2026-01-19 17:33:58 -0600

    Add Vercel Analytics for visitor tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import StopTheLoop from '../StopTheLoop.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StopTheLoop />
+    <Analytics />
   </React.StrictMode>
 );
